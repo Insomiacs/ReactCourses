37b8e9166c27f12a1e7180cfc698514108fab32e
unknown <storm17.bf@gmail.com>
2019-03-10 15:03:48 +0300

    add proptypes to stateless component

--- src/components/ReactStatelessComponentExample/ReactStatelessComponentExample.jsx
+++ src/components/ReactStatelessComponentExample/ReactStatelessComponentExample.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ReactStatelessComponentExample = ({ text }) =>
     <div>{text}</div>
 ;
 
 export default ReactStatelessComponentExample;
+
+ReactStatelessComponentExample.propTypes = {
+    text: PropTypes.string,
+};
