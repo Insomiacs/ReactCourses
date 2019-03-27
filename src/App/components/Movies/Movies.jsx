85e92ffcf5e1abfbf3e98f8cda4d8be5aa6a15f4
unknown <storm17.bf@gmail.com>
2019-03-27 10:51:50 +0300

    move movie component into movies

--- src/App/components/Movies/Movies.jsx
+++ src/App/components/Movies/Movies.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-import Movie from '../Movie';
+import Movie from './Movie';
 const Movies = ({movies}) => (
     <Fragment>
         {
